c7f06d6068b882a8b222565b1d7315e1d27168d6
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2025-02-01 20:25:47 -0600

    created particle class

---
+++ Particle.js
@@ -0,0 +1,11 @@
+class Particle{
+    constructor(position){
+        this.position = position;
+        this.prevPosition = position;
+        this.velocity = Vector2.Zero();
+        this.color = "#28b0ff";
+    }
+
+    
+
+}
